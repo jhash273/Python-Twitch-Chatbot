2f69781bf74dce9cd29faf20b4ad7c333154aebb
Justin <justinhash273@gmail.com>
2019-08-16 08:55:57 -0400

    Create config file

---
+++ config.py
@@ -0,0 +1,10 @@
+# Contains bot configuration info
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+USER = "somename" # Insert twitch username here
+PASS = "oauth" # Insert twitch oauth IRC password from https://twitchtools.com/chat-token
+CHAN = "twitchchannel" # Insert twitch channel name to be connected to NOT THE URL
+RATE = (20/30) # Messages per second
+
+oplist = []
